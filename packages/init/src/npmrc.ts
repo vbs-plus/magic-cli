cf343ee5530cd21504c41bca80808e4332441e11
yzh990918 <251205668@qq.com>
2022-11-02 16:05:31 +0800

    fix: fix pnpm install hoist bug

---
+++ packages/init/src/npmrc.ts
@@ -0,0 +1,3 @@
+export const npmrcContent = `shamefully-hoist=true
+strict-peer-dependencies=false
+`
--- packages/init/src/template.ts
+++ packages/init/src/template.ts
@@ -8,6 +8,7 @@ import ejs from 'ejs'
 import { execaCommand } from 'execa'
 import ora from 'ora'
 import { fileContent } from './gitignore'
+import { npmrcContent } from './npmrc'
 import type { TemplateListItem } from '@vbs/magic-cli-templates'
 import type { ProjectInfo } from './type'
 
@@ -104,7 +105,7 @@ export async function renderTemplate(template: TemplateListItem, projectInfo: Pa
 
   try {
     info('🔫 正在执行依赖安装命令...')
-    fse.writeFileSync(path.resolve(targetPath, '.npmrc'), 'strict-peer-dependencies = false')
+    fse.writeFileSync(path.resolve(targetPath, '.npmrc'), npmrcContent)
     fse.writeFileSync(path.resolve(targetPath, '.gitignore'), fileContent)
     await execaCommand(installCommand, { stdio: 'inherit', encoding: 'utf-8', cwd: targetPath })
   } catch (error: any) {
