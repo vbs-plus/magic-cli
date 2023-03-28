225f1e3aa3cfe20b8d112e6ccdaaf562f66ef7b8
yzh990918 <251205668@qq.com>
2023-03-28 19:18:00 +0800

    fix(template): __dirname is not defined in ES module scope

--- packages/templates/src/enum.ts
+++ packages/templates/src/enum.ts
@@ -1,6 +1,10 @@
 
 import path from 'path'
+import { fileURLToPath } from 'url'
 import dotenv from 'dotenv'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 dotenv.config({
   path: path.resolve(__dirname, '../../../.env'),
